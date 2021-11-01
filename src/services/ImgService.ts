60a3b3046545a7cfc422117e6c3efc6b311400da
Evgeniy <evgeniy.sapov@gmail.com>
2021-11-01 07:48:55 +0500

    created img service

---
+++ src/services/ImgService.ts
@@ -0,0 +1,37 @@
+export interface imgDataAPI {
+	id:string;
+	alt_description?:string;
+	urls: {
+		regular:string;
+	}
+}
+
+export class ImgService<T> {
+	public readonly _apiBaseUrl:string;
+
+	constructor() {
+		this._apiBaseUrl = 'https://api.unsplash.com/';
+	}
+
+	getResource = async (url:string):Promise<Response> => {
+		const res = await fetch(`${this._apiBaseUrl}${url}`,{
+			headers:{
+				Authorization:`Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+			}
+		});
+		
+		if (!res.ok) {
+			throw new Error(
+				`Could not fetch ${url}` + `, received ${res.status}`
+			);
+		}
+		return res;
+	};
+
+	getPhotos = async (amount:number):Promise<T> =>{
+		const res = await this.getResource(`photos/random?count=${amount}`);
+		return res.json();
+	}
+}
+
+export const imgAPI = new ImgService<imgDataAPI[]>();
