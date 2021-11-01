c20d0329b04a87ecfb85f05a165538f847985ebd
Evgeniy <evgeniy.sapov@gmail.com>
2021-11-01 10:05:10 +0500

    added game logic

--- src/store/slices/game.slice.ts
+++ src/store/slices/game.slice.ts
@@ -1,26 +1,63 @@
+import { imgDataAPI } from './../../services/ImgService';
 import { ICard } from './../../components/Card/Card.props';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { imgAPI } from '../../services/ImgService';
+import { createGameField } from '../../utils/utils';
 
 export interface GameState {
+	id:number;
 	isPlayable:boolean,
+	isFirstGame:boolean,
 	pairsFound:number,
 	chosenCard:undefined | ICard,
 	Cards: ICard[]
 }
 
 const initialState: GameState = {
+	id:Date.now(),
+	isFirstGame:true,
 	isPlayable: false,
 	pairsFound:0,
 	chosenCard: undefined,
 	Cards:[],
 };
 
+export const fetchImgs= createAsyncThunk('gameState/fetchImgs',
+	async ()=> {
+		const res = await imgAPI.getPhotos(18);
+		return res;
+	});
+
 const gameStateSlice = createSlice({
 	name:'gameState',
 	initialState,
 	reducers: {
+		restartGame:(state)=>{
+			state.id=Date.now();
+			state.pairsFound=0;
+			state.chosenCard=undefined;
+		},
+		firstGameEnded:(state)=>{
+			state.isFirstGame=false;
+		},
 		setCards:(state,action:PayloadAction<ICard[]>)=>{
 			state.Cards = action.payload;
+		},
+		flipCard:(state,action:PayloadAction<ICard>)=>{
+			state.Cards=state.Cards.map((prevCard: ICard) =>
+				(prevCard.id === action.payload.id ? { ...prevCard, isFlipped: true } : prevCard));
+		},
+		flipCardBack:(state,action:PayloadAction<ICard>)=>{
+			state.Cards=state.Cards.map((prevCard: ICard) =>
+				(prevCard.id === action.payload.id ? { ...prevCard, isFlipped: false } : prevCard));
+		},
+		flipPairsBack:(state,action:PayloadAction<ICard>)=>{
+			state.Cards=state.Cards.map((prevCard: ICard) =>
+				(prevCard.pairId === action.payload.pairId ? { ...prevCard, isFlipped: false } : prevCard));
+		},
+		flipDifferentBack:(state,action:PayloadAction<ICard>)=>{
+			state.Cards=state.Cards.map((prevCard: ICard) =>
+				( prevCard.id === action.payload.id ||  prevCard.pairId === action.payload.pairId ? { ...prevCard, isFlipped: false } : prevCard));
 		},
 		changeGameState:(state)=>{
 			state.isPlayable = !state.isPlayable;
@@ -34,6 +71,17 @@ const gameStateSlice = createSlice({
 		resetChosenCards:(state)=>{
 			state.chosenCard = undefined;
 		}
+	},
+	extraReducers: {
+		[fetchImgs.pending.type]:(state:GameState)=>{
+			state.Cards=[];
+		},
+		[fetchImgs.fulfilled.type]:(state:GameState,action:PayloadAction<imgDataAPI[]>)=>{
+			state.Cards = createGameField(action.payload);
+		},
+		[fetchImgs.rejected.type]:(state:GameState)=>{
+			state.Cards=[];
+		},
 	}
 });
 
@@ -42,5 +90,10 @@ export const {
 	incrementFoundPairs,
 	choseCard,
 	resetChosenCards,
-	setCards} = gameStateSlice.actions;
+	flipCardBack,
+	flipCard,flipPairsBack,
+	flipDifferentBack,
+	setCards,
+	restartGame,
+	firstGameEnded} = gameStateSlice.actions;
 export default gameStateSlice.reducer;
